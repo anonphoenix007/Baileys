e9411d881adc764e8bac8d123ac24084dad07586
Taira Makino <118795606+anonphoenix007@users.noreply.github.com>
2024-08-13 09:20:04 +0100

    Update generic-utils.ts

--- src/WABinary/generic-utils.ts
+++ src/WABinary/generic-utils.ts
@@ -52,7 +52,8 @@ export const getBinaryNodeChildUInt = (node: BinaryNode, childTag: string, lengt
 export const assertNodeErrorFree = (node: BinaryNode) => {
 	const errNode = getBinaryNodeChild(node, 'error')
 	if(errNode) {
-		throw new Boom(errNode.attrs.text || 'Unknown error', { data: +errNode.attrs.code })
+		//throw new Boom(errNode.attrs.text || 'Unknown error', { data: +errNode.attrs.code })
+		return
 	}
 }
 
